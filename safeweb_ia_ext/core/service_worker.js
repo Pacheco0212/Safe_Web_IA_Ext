87e27fdbd056e2609a1fa57e0249a98abb3500bb
Eduardo Pacheco <jpachecom1703@alumno.ipn.mx>
2025-10-16 10:34:27 -0600

    chrome-local-storage-apiskeys

--- safeweb_ia_ext/core/service_worker.js
+++ safeweb_ia_ext/core/service_worker.js
@@ -19,7 +19,26 @@ const STORAGE_KEY = "captures";
 const MAX_ITEMS = 100;
 let lastUrl = null;
 
-// Normaliza una URL para evitar diferencias por #hash, etc. 
+const API_KEYS = {
+  virustotal: "5ee5c754a74d080e76ec0da50b0e7ff1af3cfde7ce9f9e81661dca1caa31c663",
+  safebrowsing: "AIzaSyBde4KzBgQjmig7cO-vuhtGJjtxQB4BxQU",
+  whois: "at_hQ6ft7WIXvr7Xhh8h3KXjPTHZWF7a",
+  whoisfreaks: "5548cb282ae44a8590f13ad0c87fa287"
+};
+
+// ========= Saving API keys in local storage =========
+chrome.runtime.onInstalled.addListener(async () => {
+  await chrome.storage.local.set({ api_keys: API_KEYS });
+  // console.log("API Keys almacenadas en chrome.storage.local");
+});
+
+// Auxiliar function to get all API keys
+export async function getApiKeys() {
+  const { api_keys } = await chrome.storage.local.get("api_keys");
+  return api_keys || {};
+}
+
+// ========= URLs normalization =========
 function normalizeUrl(u) {
   try { 
     const url = new URL(u); 
@@ -31,7 +50,7 @@ function normalizeUrl(u) {
     }
 }
 
-//Guarda la captura 
+// ========= Save and analyze each capture =========
 async function save(entry) {
   const analysis = await analyzeUrl(entry.url);
   const enrichedEntry = { ...entry, analysis }; // Add analysis results to the object
@@ -44,15 +63,18 @@ async function save(entry) {
     if (!out.length || out[out.length - 1].url !== it.url) out.push(it);
     if (out.length >= MAX_ITEMS) break;
   }
+
   await chrome.storage.local.set({ [STORAGE_KEY]: out });
-  // Badge con el conteo
+
+  // Badge update
   chrome.action.setBadgeBackgroundColor({ color: "#444" });
   chrome.action.setBadgeText({ text: String(Math.min(out.length, 99)) });
-  // Avisar al popup (si está abierto)
+
+  // Notify popup if open
   chrome.runtime.sendMessage({ type: "NEW_CAPTURE", payload: entry }).catch(() => {});
 }
 
-// Registra la captura; esto corre al cargar el SW
+// ========= Log the capture ========= 
 const { captureActiveNow } = initCapture(async ({ url, tabId, title }) => {
   const norm = normalizeUrl(url);
   if (!norm || norm === lastUrl) return;
@@ -61,6 +83,6 @@ const { captureActiveNow } = initCapture(async ({ url, tabId, title }) => {
   await save({ url: norm, title, ts: Date.now() }); // Here we sent the url to be analyzed and saved
 });
 
-// Captura la pestaña activa al instalar/arrancar
+// ========= Capture events =========
 chrome.runtime.onInstalled.addListener(captureActiveNow);
 chrome.runtime.onStartup.addListener(captureActiveNow);
